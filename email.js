197cabff4e9c68862a0393bf2393992d2c974c14
TheMixas <themixaslt@gmail.com>
2023-12-17 22:28:01 +0000

    configuring email stmp ports

--- email.js
+++ email.js
@@ -3,7 +3,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port:25,
+
+    port: 465,
     secure: true,
     auth: {
         // TODO: replace `user` and `pass` values from <https://forwardemail.net>
